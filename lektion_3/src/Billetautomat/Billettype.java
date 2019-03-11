262fff4b51130652b0968a7fecd0c14d71bd1f0a
Nikolaj Landberg <Nikolaj Landberg@LAPTOP-PLFGULU9>
2019-03-11 20:02:59 +0100

    Integrated Zone types with Ticket types.

--- lektion_3/src/Billetautomat/Billettype.java
+++ lektion_3/src/Billetautomat/Billettype.java
@@ -12,83 +12,93 @@ import java.util.Scanner;
  * @author Nikolaj Landberg
  */
 public class Billettype {
+
     Billetautomat automat;
     Zonetyper zone = new Zonetyper();
-      Scanner s = new Scanner(System.in);
+    Scanner s = new Scanner(System.in);
     private int voksenpris;
     private int boernepris;
     private int cykelpris;
-    
-    public Billettype(){
+    private int zonetype;
+
+    public Billettype() {
         voksenpris = 24;
         boernepris = 12;
         cykelpris = 30;
     }
-    
-    
-    public void setPrice( String montoerkode){
-        if (montoerkode.equals(automat.getPassword())){
-        boolean isDone = false;
-        int choice = -1;
-        
-        while (isDone != true){
-            System.out.println("Tryk 1 for at saette voksen billet pris\nTryk 2 for at sætte børne billet pris\ntryk 3 for at sættte cykel billetpris\ntryk 0 for at afslutte.");
-            choice = s.nextInt();
-            switch (choice){
-                case 1:
-                    System.out.println("Saet voksen billetpris:");
-                    voksenpris = s.nextInt();
-                    break;
-                case 2:
-                    System.out.println("Saet boerne billetpris:");
-                    boernepris = s.nextInt();
-                    break;
-                case 3:
-                    System.out.println("Saet cykel billetpris:");
-                    cykelpris = s.nextInt();
-                    break;
-                case 0:
-                    isDone = true;
-                    break;
+
+    public void setZoneType(int index) {
+        zonetype = index;
+    }
+
+    public void setPrice(String montoerkode) {
+        if (montoerkode.equals(automat.getPassword())) {
+            boolean isDone = false;
+            int choice = -1;
+
+            while (isDone != true) {
+                System.out.println("Tryk 1 for at saette voksen billet pris\nTryk 2 for at sætte børne billet pris\ntryk 3 for at sættte cykel billetpris\ntryk 0 for at afslutte.");
+                choice = s.nextInt();
+                switch (choice) {
+                    case 1:
+                        System.out.println("Saet voksen billetpris:");
+                        voksenpris = s.nextInt();
+                        break;
+                    case 2:
+                        System.out.println("Saet boerne billetpris:");
+                        boernepris = s.nextInt();
+                        break;
+                    case 3:
+                        System.out.println("Saet cykel billetpris:");
+                        cykelpris = s.nextInt();
+                        break;
+                    case 0:
+                        isDone = true;
+                        break;
+                }
             }
+
+        } else {
+            System.err.println("Kunne ikke saette pris - forkert kode");
         }
-        
-        } else System.err.println("Kunne ikke saette pris - forkert kode");
     }
-    public void udskrivVoksenBillet(){
+
+    public void udskrivVoksenBillet() {
         System.out.println("##########B##T##########");
         System.out.println("# Borgen Trafikselskab #");
-	System.out.println("#                      #");
-	System.out.println("#    Voksen Billet     #");
-        System.out.println("#    antal "+ zone+"     #");
-	System.out.println("#        " + voksenpris + " kr.        #");
-	System.out.println("#                      #");
-	System.out.println("# Du har " + automat.getBalance() + " kr til gode #");
-	System.out.println("##########B##T##########");
-	System.out.println();
+        System.out.println("#                      #");
+        System.out.println("#    Voksen Billet     #");
+        System.out.println("#    " + zone.getZoneType(zonetype) + ": " + zone.getZonePrice(zonetype) + "     #");
+        System.out.println("#        " + (voksenpris + zone.getZonePrice(zonetype)) + " kr.        #");
+        System.out.println("#                      #");
+        System.out.println("# Du har " + automat.getBalance() + " kr til gode #");
+        System.out.println("##########B##T##########");
+        System.out.println();
     }
-    
-    public void udskrivBoerneBillet(){
+
+    public void udskrivBoerneBillet() {
         System.out.println("##########B##T##########");
         System.out.println("# Borgen Trafikselskab #");
-	System.out.println("#                      #");
-	System.out.println("#    Boerne Billet     #");
-	System.out.println("#        " + boernepris + " kr.        #");
-	System.out.println("#                      #");
-	System.out.println("# Du har " + automat.getBalance() + " kr til gode #");
-	System.out.println("##########B##T##########");
-	System.out.println();
+        System.out.println("#                      #");
+        System.out.println("#    Boerne Billet     #");
+        System.out.println("#    " + zone.getZoneType(zonetype) + ": " + zone.getZonePrice(zonetype) + "     #");
+        System.out.println("#        " + (boernepris + zone.getZonePrice(zonetype)) + " kr.        #");
+        System.out.println("#                      #");
+        System.out.println("# Du har " + automat.getBalance() + " kr til gode #");
+        System.out.println("##########B##T##########");
+        System.out.println();
     }
-    
-    public void udskrivCykelBillet(){
+
+    public void udskrivCykelBillet() {
         System.out.println("##########B##T##########");
         System.out.println("# Borgen Trafikselskab #");
-	System.out.println("#                      #");
-	System.out.println("#    Voksen Billet     #");
-	System.out.println("#        " + cykelpris + " kr.        #");
-	System.out.println("#                      #");
-	System.out.println("# Du har " + automat.getBalance() + " kr til gode #");
-	System.out.println("##########B##T##########");
-	System.out.println();
+        System.out.println("#                      #");
+        System.out.println("#     Cykel Billet     #");
+        System.out.println("#    " + zone.getZoneType(zonetype) + ": " + zone.getZonePrice(zonetype) + "     #");
+        System.out.println("#        " + (cykelpris + zone.getZonePrice(zonetype)) + " kr.        #");
+        System.out.println("#                      #");
+        System.out.println("# Du har " + automat.getBalance() + " kr til gode #");
+        System.out.println("##########B##T##########");
+        System.out.println();
     }
 }
