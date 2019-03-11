a6710f75e31d8f6dd73d4a6e0a9ddedb16cd0b06
Jeppe <Jeppe@DESKTOP-AA8M3D2>
2019-03-11 16:30:18 +0100

    Log opsat

---
+++ lektion_3/src/Billetautomat/Log.java
@@ -0,0 +1,15 @@
+
+package Billetautomat;
+
+
+public class Log {
+    
+    private String tidspunkt = "";
+    private String handling = "";
+    private String billettype = "";
+    private int beløb = 0;
+
+}
+
+
+
