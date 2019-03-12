db391a5d30662fc053477a7087b879c5df96c0b7
Jeppe <Jeppe@DESKTOP-AA8M3D2>
2019-03-12 19:05:35 +0100

    Konstruktør for log

--- lektion_3/src/Billetautomat/Log.java
+++ lektion_3/src/Billetautomat/Log.java
@@ -1,10 +1,19 @@
 
 package Billetautomat;
 
+import java.util.Date;
+
 
 public class Log {
+
+    public Log(String handling, String billettype, int beløb) {
+        this.handling = handling;
+        this.billettype = billettype;
+        this.beløb = beløb;
+        
+    }
     
-    private String tidspunkt = "";
+    private Date tidspunkt = new Date();
     private String handling = "";
     private String billettype = "";
     private int beløb = 0;
