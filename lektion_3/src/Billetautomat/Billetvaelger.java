0aac9b0a75094e67961860c833501a3bc5e19be7
Valdemar Landberg <Valdemar Landberg@LAPTOP-PLFGULU9>
2019-03-14 12:46:27 +0100

    Added remove functionality

--- lektion_3/src/Billetautomat/Billetvaelger.java
+++ lektion_3/src/Billetautomat/Billetvaelger.java
@@ -9,49 +9,69 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Billetvaelger {
-    class Billet{
-     String typename;
-     String zonename;
-     int price;
-     
-     public Billet(String type, String zone, int sumprice){
-         this.typename = type;
-         this.zonename = zone;
-         this.price = sumprice;
-     }
-     
+
+    class Billet {
+
+        String typename;
+        String zonename;
+        int price;
+
+        public Billet(String type, String zone, int sumprice) {
+            this.typename = type;
+            this.zonename = zone;
+            this.price = sumprice;
+        }
+
     }
-    
+
     Scanner s = new Scanner(System.in);
     Billettype type = new Billettype();
     ArrayList<Billet> liste = new ArrayList<Billet>();
-    
+
     private int antalbillet;
-    
-    public Billetvaelger(){
+
+    public Billetvaelger() {
         antalbillet = 0;
     }
-    public void addBillet(int typeindex, int zoneindex){
-       
-       String typename = type.getBilletType(typeindex);
-       String zonename = type.getZoneType(zoneindex);
-       int price = type.getBilletPrice(typeindex) + type.getZonePrice(zoneindex);
-        
+
+    public void addBillet(int typeindex, int zoneindex) {
+
+        String typename = type.getBilletType(typeindex);
+        String zonename = type.getZoneType(zoneindex);
+        int price = type.getBilletPrice(typeindex) + type.getZonePrice(zoneindex);
+
         Billet x = new Billet(typename, zonename, price);
-       liste.add(x);
-       antalbillet++;
-       
+        liste.add(x);
+        antalbillet++;
+
     }
-    public ArrayList<Billet> getTicketList(){
+
+    public ArrayList<Billet> getTicketList() {
         return liste;
     }
-    public void billetVaelgerUI(){
-        
-        
+
+    public void removeBillet(int typeindex, int zoneindex, int amount) {
+
+        if (amount > 0) {
+            String typename = type.getBilletType(typeindex);
+            String zonename = type.getZoneType(zoneindex);
+            int price = type.getBilletPrice(typeindex) + type.getZonePrice(zoneindex);
+
+            Billet x = new Billet(typename, zonename, price);
+            for (int i = 0; i < amount; i++) {
+                liste.remove(x);
+
+            }
+        }
+
     }
-    
-    public void antalBilletter(int antal){
-        
+
+    public void billetVaelgerUI() {
+
     }
-    
+
+    public void antalBilletter(int antal) {
+
+    }
+
 }
