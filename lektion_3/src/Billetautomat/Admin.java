8febddb7cbbce17a1c54b7ec2d6d138720800b21
Jeppe <Jeppe@DESKTOP-AA8M3D2>
2019-03-17 19:51:43 +0100

    translog i admin

--- lektion_3/src/Billetautomat/Admin.java
+++ lektion_3/src/Billetautomat/Admin.java
@@ -4,54 +4,54 @@
  * and open the template in the editor.
  */
 package Billetautomat;
+
 import Billetautomat.*;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Admin {
-   boolean runAd;
-   int choice;
-   Billetautomat automat;
-   Billetvaelger bilvalg;
-   Scanner adminscan = new Scanner(System.in);
-   
-   public Admin (Billetvaelger bv, Billetautomat a){
+
+    boolean runAd;
+    int choice;
+    Billetautomat automat;
+    Billetvaelger bilvalg;
+    Scanner adminscan = new Scanner(System.in);
+
+    public Admin(Billetvaelger bv, Billetautomat a) {
         bilvalg = bv;
         automat = a;
     }
-   
-   public void AdminUI(){
-       runAd = false;
-       while(runAd != true){
-       System.out.println("1 to set price\n2 to set zone price\n0 to end Admin");
-       choice = adminscan.nextInt();
-        
-       switch(choice){
-            case 1:
-               setPrice();
-               break;
-            case 2:
-                setZonePrice();
-                break;
-            case 0:
-                runAd = true;
-                break;
-       }
-   }   
-   }
-   
-    public void setPrice(){
-       System.out.println(automat.isAdmin());
-        bilvalg.setPrice();
-   
-   
-    
+
+    public void AdminUI() {
+        runAd = false;
+        while (runAd != true) {
+            System.out.println("1 to set price\n2 to set zone price\n0 to end Admin");
+            choice = adminscan.nextInt();
+
+            switch (choice) {
+                case 1:
+                    setPrice();
+                    break;
+                case 2:
+                    setZonePrice();
+                    break;
+                case 3:
+                    automat.printTransaktion();
+                case 0:
+                    runAd = true;
+                    break;
+            }
+        }
     }
-     public void setZonePrice(){
-       
+
+    public void setPrice() {
+        System.out.println(automat.isAdmin());
+        bilvalg.setPrice();
+
+    }
+
+    public void setZonePrice() {
+
         bilvalg.setZonePrice();
-   
-   
-    
+
     }
 }
